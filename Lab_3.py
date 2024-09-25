424bd9e16c22cefc00642f1d1eb35570a440e669
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-09-25 06:46:46 -0700

    lab_3

--- Lab_3.py
+++ Lab_3.py
@@ -2,5 +2,22 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categoricalz
 
+
+with np.load("mnist.npz")  as data :
+    x_train = data["x_train"]
+    y_train = data["y_train"]
+    x_test = data["x_test"]
+    y_test = data["y_test"]
+
+
+type(x_train)
+
+x_train.shape
+
+img_1 = x_train[100]
+x_train[500].shape
+
+import matplotlib.pyplot as plt
+plt.imshow(img_1)
