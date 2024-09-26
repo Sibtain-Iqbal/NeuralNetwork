e64b1e2fd152bcc5c600d9610dd310385205f305
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-09-26 06:44:02 -0700

    Create Lab_3.py

---
+++ Lab_3.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
